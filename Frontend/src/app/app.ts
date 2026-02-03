6a8f4821a04161b0024739858fa241862b03c10d
Julitown46 <morenojulian4502@gmail.com>
2026-02-03 14:36:31 +0100

    build: initial structure

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Toast } from './components/toast/toast';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet, Toast, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Frontend');
+  protected readonly title = signal('Frontend RealStateLegacy');
 }
