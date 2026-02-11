7fece01cbf539edc9f331b0dd3a25d3c6cb0e060
Julitown46 <morenojulian4502@gmail.com>
2026-02-11 13:45:25 +0100

    build: footer done and some fixes in style

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Toast } from './components/toast/toast';
 import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toast, Navbar],
+  imports: [RouterOutlet, Toast, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ Frontend/src/app/components/houses/houses.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-houses',
+  imports: [],
+  templateUrl: './houses.html',
+  styleUrl: './houses.css',
+})
+export class Houses {
+
+}
